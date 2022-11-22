f0973fc5e370c4e13db4e46f66b2fd74276a4c5f
tavaresmateus <mateustavares123@gmail.com>
2022-11-22 19:15:53 -0300

    login 1.0

--- src/screens/Login/styles.ts
+++ src/screens/Login/styles.ts
@@ -1,11 +1,62 @@
-import { StyleSheet} from "react-native";
+import { Platform, StyleSheet,} from "react-native";
 
 export const styles = StyleSheet.create({
     container: {
-        flex:1,
-        backgroundColor: '#121015',
-        paddingTop: 60,
-        paddingHorizontal: 30
+      flex: 1,
+      backgroundColor: '#FFF',
+      alignItems: 'center',
+    },
+    image:{
+        width: 370,
+        height: 200
+    },
+    title:{
+        fontFamily: '',
+        fontWeight: '300'
+    },
+    viewLogin:{
+        padding: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    inputLogin:{
+        marginBottom: 20,
+        padding: 10,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#CDCDCD',
+        width: 300
+    },
+    buttonLogin:{
+        alignItems: 'center',
+        padding: 10,
+        backgroundColor: '#34A853',
+        width: 300,
+        borderRadius: 10
+    },
+    textButtonLogin:{
+        color: 'white',
+        fontSize: 20
+    },
+    textSignup:{
+        color: 'grey',
+        margin: 10
+    },
+    boxAltLogin:{
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        padding: 5,
+        width: 300
+    },
+    footer:{
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    registerText:{  
+        marginTop: 10,
+        color: 'grey',
+    },
+    registerTextButton:{
+        fontWeight: '600'
     }
-
-})
+});
