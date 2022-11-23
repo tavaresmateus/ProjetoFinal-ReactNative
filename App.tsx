dced8b9aa0082128694c3445a55e22055e853ff8
HectorBonilhaa <hectoroliveira1@gmail.com>
2022-11-23 11:44:36 -0300

    testando getAll

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 
 import React from "react";
+import { Explore } from "./src/screens/Explore";
 import { Login } from './src/screens/Login';
+
 
 export default function App() {
   return (
-    <Login />
+    <Explore />
   )
 
 }
