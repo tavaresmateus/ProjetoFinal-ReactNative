37c8cb5406308b977bdcaadfd42da03c51abd320
FranciscoEATorres <dudueco@yahoo.com.br>
2022-11-23 14:39:49 -0200

    paginaProdutos

--- src/screens/Products/index.tsx
+++ src/screens/Products/index.tsx
@@ -1,1 +1,62 @@
-import react from react;
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  ScrollViewComponent,
+  ImageBackground,
+} from "react-native";
+import { styles } from "../Products/styles";
+import mengo from "../../assets/imagens/fla1.png";
+import mengao from "../../assets/imagens/fla2.png";
+import { AntDesign } from "@expo/vector-icons";
+
+export const Products = () => {
+  return (
+
+    
+    <View style={styles.container}>
+        
+      <ImageBackground
+        style={styles.bola1}
+        source={require("../../assets/elipse.png")}
+      >
+        <Text style={styles.text}>Produtos</Text>
+        <View style={styles.searchbar}>
+          <TextInput
+            style={styles.input}
+            keyboardType={"default"}
+            placeholder="Search"
+          />
+          <AntDesign name="search1" size={30} color="black" />
+        </View>
+      
+
+      <View style={styles.colunas}>
+        <Image style={styles.imagePost} source={mengo} />
+      </View>
+
+      <View style={styles.colunas}>
+        <Image style={styles.imagePost2} source={mengo} />
+      </View>
+
+      <View style={styles.colunas}>
+        <Image style={styles.imagePost2} source={mengo} />
+      </View>
+
+      <View style={styles.colunas}>
+        <Image style={styles.imagePost2} source={mengo} />
+      </View>
+
+      
+      <View style={styles.colunas}>
+        <Image style={styles.imagePost2} source={mengao} />
+      </View>
+      </ImageBackground>
+      
+      <Text style={styles.text2}>Favoritos</Text>
+    </View>
+  );
+};
