b275dc7b2eb6173aa4c7d47c7093596e43fb03f5
FranciscoEATorres <dudueco@yahoo.com.br>
2022-11-23 18:17:02 -0200

    criandoModal

--- App.tsx
+++ App.tsx
@@ -3,10 +3,14 @@ import { Explore } from "./src/screens/Explore";
 import { Login } from './src/screens/Login';
 import Route from "./src/routes";
 import { Register } from "./src/screens/Register";
+import { ModalProduct } from "./src/components/Modais/ModalStats"
+import { Products } from "./src/screens/Products";
 
 export default function App() {
   return (
-    <Route/>
+    // <Route/>
+    // <ModalProduct/>
+    <Products/>
   )
 
 }
